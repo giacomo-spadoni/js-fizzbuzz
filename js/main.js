0406a757ae9330c444d9137054256f9caeeef461
giacomo-spadoni <giaky.spado@gmail.com>
2024-04-21 19:52:16 +0200

    added some toggle effect

--- js/main.js
+++ js/main.js
@@ -4,18 +4,25 @@ const mySection = document.querySelector('section');
 for (let count = 1; count <= 100; count++) {
     // creo la costante che mi aggiungerà un div
     const newDiv = document.createElement('div');
+    const divToggle = newDiv.addEventListener('click', function () {
+        this.classList.toggle('test')
+    })
     // creo la condizione if se le il numero è divisibile per 3 o 5 o entrambi
     if (count % 3 === 0 && count % 5 === 0) {
         newDiv.textContent = 'Fizz-Buzz';
         newDiv.classList.add('fizz-buzz');
+        divToggle
     } else if (count % 5 === 0) {
         newDiv.textContent = 'Buzz';
         newDiv.classList.add('buzz');
+        divToggle
     } else if (count % 3 === 0) {
         newDiv.textContent = 'Fizz';
         newDiv.classList.add('fizz');
+        divToggle
     } else {
         newDiv.textContent = count;
+        divToggle
     }
     // "appendo" il div dentro la section
     mySection.append(newDiv);
